f908191df02fc23a2319a4b58e9d9508ca4dd9e3
Krzysztof S. Matejak <krzysztof@matejak.com>
2023-08-09 14:52:22 +0200

    ng content projection

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -7,21 +7,14 @@ import { Quotation } from '../models/quotation';
   styleUrls: ['./form.component.css']
 })
 export class FormComponent {
-  
   @Output() newQuotation = new EventEmitter<Quotation>()
-
-  // Pole do ukrywania i pokazywania formularza
-  showForm = true;
-
-  // pole quotation reprezentuje pojedynczy cytat
+  showForm = false;
   quotation: Quotation = { author: '', sentence: '', votes: 0 };
 
-  // przełącza pole klasy true / false
   onSwitchForm(): void {
     this.showForm = !this.showForm;
   }
 
-  // dodaje cytat na początek listy i resetuje pole quotation
   addQuotation() {
     this.newQuotation.emit(this.quotation);
     this.quotation = { author: '', sentence: '', votes: 0 };
